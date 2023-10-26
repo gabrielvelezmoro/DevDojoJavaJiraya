049b6a5a48cdce593a0d941e474f227ae5493bd3
Gabriel Mauricio Velezmoro <83468072+gabrielvelezmoro@users.noreply.github.com>
2023-10-26 16:44:51 -0300

    operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -28,5 +28,40 @@ public class Aula04Operadores {
         System.out.println("isDezDiferentelDez "+isDezDiferentelDez);
         System.out.println("isDezMaiorIgualAVinte "+isDezMaiorIgualAVinte);
         System.out.println("isDezMenorIgualAVinte "+isDezMenorIgualAVinte);
+
+        // &&(AND) ||(Or) !(not);
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+ isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+ isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel "+ isPlaystationCincoCompravel);
+
+
+        // += -= *= /+ %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+
+        System.out.println("bonus: " + bonus);
+
+        int contador = 0;
+        contador +=1;
+        contador++;
+
+        System.out.println("contador: " + contador);
+
+        int contador2 = 0;
+        System.out.println("contador2: " + contador2++);
     }
 }
