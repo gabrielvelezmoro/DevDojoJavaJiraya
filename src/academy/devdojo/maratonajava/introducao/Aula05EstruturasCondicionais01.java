2a8cc9aec4863d41bd7939a2069505ba5e660535
gabri <gabrieuvelezmoro@gmail.com>
2023-12-01 15:30:01 -0300

    Segunda aula de condicionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais01 {
+    public static void main(String[] args) {
+        int idade = 12;
+        boolean isAutorizadoComprarBebidas = idade >= 18;
+
+        if (isAutorizadoComprarBebidas != false){
+            System.out.println("Autorizado a comprar bebida alcoolica");
+        }else{
+            System.out.println("Não Autorizado a comprar bebida alcoolica");
+        }
+
+        //if(!isAutorizadoComprarBebidas){
+        //}
+    }
+}
