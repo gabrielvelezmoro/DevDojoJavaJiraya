354703ef8601dac38d2761f38204be5b64428011
gabri <gabrieuvelezmoro@gmail.com>
2023-12-04 17:22:27 -0300

    Refatora Exercicio: Sexta aula e formatações

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasCondicionais06.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasCondicionais06.java
@@ -5,29 +5,23 @@ public class Aula06EstruturasCondicionais06 {
         // Utilizando switch e dados os valores de 1 a 7, imprima se é dia útil ou final de semana
         // Considerando 1 como domingo
 
-        int dia = 0;
+        int dia = 3 ;
 
         switch (dia) {
             case 1:
                 System.out.println("Final de semana");
                 break;
-            case 2:
+            case 7:
                 System.out.println("Dia de semana");
                 break;
             case 3:
-                System.out.println("Dia de semana");
-                break;
+
             case 4:
-                System.out.println("Dia de semana");
-                break;
+
             case 5:
-                System.out.println("Dia de semana");
-                break;
+
             case 6:
                 System.out.println("Dia de semana");
-                break;
-            case 7:
-                System.out.println("Final de semana");
                 break;
             default:
                 System.out.println("input error");
